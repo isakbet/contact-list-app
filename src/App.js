8786673f2e0a95e7faec49e24aab530c5abca4ee
Yoseph Berhane <yosephbet@gmail.com>
2020-05-24 16:36:53 +0200

    added contact card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
-function App() {
+const App = () => { 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Email: Jenny.Han@notreal.com</p>
+        <p>Age: 25</p>
+      </div>
     </div>
   );
 }
